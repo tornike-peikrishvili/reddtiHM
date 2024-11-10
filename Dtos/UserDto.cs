31330b299ef7b86c1315e243e484de2e2b7ca18c
anri <kezeroti2@gmail.com>
2024-11-11 00:46:12 +0400

    ensure migrations correctness

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -1,10 +1,14 @@
-﻿using Reddit.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Reddit.Models;
 
 namespace Reddit.Dtos
 {
     public class UserDto
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public User CreateUser() {
              return new User { Name = Name, Email = Email };
